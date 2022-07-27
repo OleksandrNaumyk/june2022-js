3e4cbf1b9dc1e91c4b08376adce4d7d1a4895b1c
mac <naumyk@ukr.net>
2022-07-27 16:51:05 +0300

    +

--- homework-2/script.js
+++ homework-2/script.js
@@ -1,50 +1,50 @@
 // - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
 
-let arr1 = ['hello', 44, 33, 22, 'okten', 655, 'school', 432, 65, 'javascript'];
-console.log(arr1);
+// let arr1 = ['hello', 44, 33, 22, 'okten', 655, 'school', 432, 65, 'javascript'];
+// console.log(arr1);
 
 // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
 
-let obj1 = {
-    title: 'Alex',
-    pageCount: 99,
-    genre: 'detective'
-}
-
-let obj2 = {
-    title: 'Natalia',
-    pageCount: 245,
-    genre: 'novel'
-}
-let obj3 = {
-    title: 'Oleh',
-    pageCount: 34,
-    genre: 'history'
-}
+// let obj1 = {
+//     title: 'Alex',
+//     pageCount: 99,
+//     genre: 'detective'
+// }
+//
+// let obj2 = {
+//     title: 'Natalia',
+//     pageCount: 245,
+//     genre: 'novel'
+// }
+// let obj3 = {
+//     title: 'Oleh',
+//     pageCount: 34,
+//     genre: 'history'
+// }
 
 // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив.
 // Кожен автор має поля name,age
 
-let obj4 = {
-    title: 'Javascript',
-    pageCount: 456,
-    genre: 'Text',
-    authors: [{name: 'Oleh', age: 44}, {name: 'Ivan', age: 42}]
-}
-
-let obj5 = {
-    title: 'Java',
-    pageCount: 432,
-    genre: 'Text',
-    authors: [{name: 'Vitaliy', age: 33}, {name: 'Ivan', age: 47}]
-}
-
-let obj6 = {
-    title: 'Python',
-    pageCount: 333,
-    genre: 'Text',
-    authors: [{name: 'Alex', age: 25}, {name: 'Natalia', age: 39}]
-}
+// let obj4 = {
+//     title: 'Javascript',
+//     pageCount: 456,
+//     genre: 'Text',
+//     authors: [{name: 'Oleh', age: 44}, {name: 'Ivan', age: 42}]
+// }
+//
+// let obj5 = {
+//     title: 'Java',
+//     pageCount: 432,
+//     genre: 'Text',
+//     authors: [{name: 'Vitaliy', age: 33}, {name: 'Ivan', age: 47}]
+// }
+//
+// let obj6 = {
+//     title: 'Python',
+//     pageCount: 333,
+//     genre: 'Text',
+//     authors: [{name: 'Alex', age: 25}, {name: 'Natalia', age: 39}]
+// }
 
 // - Створити масив з 10 об'єктами які описують сутніть "користувач".
 // Поля: name, username,password. Вивести в консоль пароль кожного користувача
@@ -102,4 +102,6 @@ let arr2 = [
     }
 ];
 
-console.log(arr2[arr2].password);
+
+console.log(arr2[0].password);
+
